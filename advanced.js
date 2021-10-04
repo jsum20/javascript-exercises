76a6a599e93a2154546a6c7e0282e39793e9467f
Jason Sum <jasonsum321@gmail.com>
2021-10-04 15:56:37 +0100

    Adding js solutions to exercises

---
+++ advanced.js
@@ -0,0 +1,82 @@
+/**
+ * These tasks are more complicated! Using combinations of arrays and objects is very common, here you will need to combine what you know about both to complete the tasks.
+ */
+
+
+unitedKingdom = [
+    {
+        name: "Scotland",
+        capital: "Edinburgh",
+        population: 5454000,
+        touristAttractions: [
+            "Stirling Castle",
+            "Loch Ness"
+        ]
+    },
+    {
+        name: "England",
+        capital: "London",
+        population: 55980000
+    },
+    {
+        name: "Wales",
+        capital: "Swansea",
+        population: 3163000,
+        touristAttractions: [
+            "Snowdonia"
+        ]
+    },
+    {
+        name: "Northern Ireland",
+        capital: "Belfast",
+        population: 1885000,
+        touristAttractions: [
+            "Titanic Museum",
+            "Giant's Causeway"
+        ]
+    }
+]
+
+
+/**
+ * Q1. Add some tourist attractions to England
+ */
+unitedKingdom[1]["toursitAttractions"] = ["Stonehenge","Chester Zoo"];
+england = unitedKingdom[1]
+
+// console.log(england);
+
+
+/**
+ * Q2. Change the capital of Wales to "Cardiff"
+ */
+unitedKingdom[2]["capital"] = "Cardiff";
+wales = unitedKingdom[2];
+
+// console.log(wales);
+
+/**
+ * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
+ */
+
+northernIrelandKeys = Object.keys(unitedKingdom[3]);
+
+// console.log(northernIrelandKeys);
+
+
+
+/**
+ * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
+ */
+scotlandPopulation = unitedKingdom[0].population;
+englandPopulation = unitedKingdom[1].population;
+walesPopulation = unitedKingdom[2].population;
+northerIrelandPopulation = unitedKingdom[3].population;
+
+if ((scotlandPopulation < englandPopulation) && (scotlandPopulation < walesPopulation) && (scotlandPopulation < northerIrelandPopulation)) {
+    console.log("The population of Scotland is the smallest");
+} else if ((scotlandPopulation > englandPopulation) && (scotlandPopulation > walesPopulation) &&  (scotlandPopulation > northerIrelandPopulation)) {
+    console.log("The population of Scotland is the biggest");
+} else {
+    console.log("The population of Scotland is somewhere in the middle");
+}
